d550b679a3567377e50df806971c32e4395eb479
letsgetthiscode <letsgetthiscode@users.noreply.github.com>
2019-02-12 11:38:00 -0400

    rdna.hpp

---
+++ src/rdna.hpp
@@ -0,0 +1,18 @@
+string randDNA(string bases,int seed,int n)
+{
+	string bases = "";
+	int seed=-1,n=-1;  
+	int min=0,max=10;
+	uniform_int_distribution<int> un(min,max)
+	
+	cout<<"Please enter a seed:";
+	cin<<seed;
+	
+	cout<<"Please enter letter bases:";
+	cin<<bases;
+	
+	cout<<"Please enter the length:";
+	cin<<n;
+	
+	for (i=0;i<0;i++)
+	cout<<un()bases[];
